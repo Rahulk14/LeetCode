00f810501e62d8fbc7544b76d0a7431ff7916ec5
Rahul kumar jha <75601942+Rahulk14@users.noreply.github.com>
2023-07-11 16:40:18 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (89.33%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,5 +1,9 @@
 class Solution {
     public int findComplement(int num) {
+        
+        if(num==0)
+            return 1;
+        
         int bitLength = (int)(Math.log(num)/Math.log(2))+1;
       
         int bitMask = (1<<bitLength)-1;
